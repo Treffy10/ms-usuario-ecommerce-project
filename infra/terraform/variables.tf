53c5a81953201ae183fca6c5eecf16d6bc10147d
Treffy10 <jeffrobert233@gmail.com>
2025-12-08 17:15:45 -0500

    variables modificadas

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -4,11 +4,6 @@ variable "aws_region" {
   description = "Región de AWS"
 }
 
-variable "terraform_bucket" {
-  type        = string
-  description = "Nombre del bucket S3 para el estado de Terraform"
-}
-
 variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
