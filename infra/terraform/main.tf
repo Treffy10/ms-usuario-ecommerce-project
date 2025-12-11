ed275e857ea0f48b923f43687fd6c1a3110a02d8
Treffy10 <jeffrobert233@gmail.com>
2025-12-11 08:26:16 -0500

    tiempo 10 min para despliegue app

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "ec2" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.ssh_allowed_cidr
+    cidr_blocks = ["0.0.0.0/0"] # Permitir SSH desde cualquier lugar
   }
 
   ingress {
